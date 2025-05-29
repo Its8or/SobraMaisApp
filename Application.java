0a81820daf9a6c7a553f1e1ff9208843d752071e
Heitor Oliveira Terto <93292451+Its8or@users.noreply.github.com>
2025-05-29 18:10:12 -0300

    refazendo tudo

--- Application.java
+++ Application.java
@@ -4,12 +4,6 @@ import java.awt.event.*;
 import java.io.*;
 
 public class Application {
-    private static final String CACHE_FILE_NAME = "Data.dat";
-
-    private static void saveData(){}
-
-    private static void loadData(){}
-
     public static void app() {
         System.out.println("Iniciando application...");
 
