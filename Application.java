1a10ca49b2a3bbc4d069b9857a9b9b43cee5da7b
Heitor Oliveira Terto <93292451+Its8or@users.noreply.github.com>
2025-05-29 15:04:39 -0300

    Change Language from Python to Java

---
+++ Application.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class Application {
+    private static final String CACHE_FILE_NAME = "Data.dat";
+
+    private static void saveData(){}
+
+    private static void loadData(){}
+
+    public static void app() {
+        System.out.println("Iniciando application...");
+
+    }
+}
